988059d0758685d5b51834ba6af3e38605ad9c61
harshit-538 <harshit.navadiya@drcsystems.com>
2025-03-11 12:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,12 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 @app.post("/upload/")
 async def upload_video(file: UploadFile = File(...)):
     """Upload video file and save it."""
-    #file_path = os.path.join(UPLOAD_DIR, file.filename)
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
 
-    #with open(file_path, "wb") as buffer:
-     #   shutil.copyfileobj(file.file, buffer)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
-    #return {"message": "Video uploaded successfully", "filename": file.filename, "file_path": file_path}
-    return {"message": "Video uploaded successfully", "filename": file.filename}
+    return {"message": "Video uploaded successfully", "filename": file.filename, "file_path": file_path}
 
 @app.get("/transcribe/")
 async def transcribe_video(file_path: str = Query(..., description="Path of the uploaded video file")):
